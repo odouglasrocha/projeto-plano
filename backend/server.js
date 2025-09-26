8bfc26abe9c10ceb2c45e35186c33e434e085b8e
odouglasrocha <odouglasrocha@gmail.com>
2025-09-26 08:37:32 -0300

    Atualização da Configuração CORS

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use(helmet());
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
     ? ['https://planing-ita.com', 'https://www.planing-ita.com'] 
-    : ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:8080'],
+    : ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:8080', 'https://planing-ita.com'],
   credentials: true
 }));
 app.use(morgan('combined'));
